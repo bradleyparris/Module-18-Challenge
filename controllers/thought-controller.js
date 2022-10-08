03b5ff6e3a6ae5166d6bae5cfcda1c9456e15562
Bradley Parris <parrisbjp@gmail.com>
2022-10-07 22:34:19 -0400

    thought controller

---
+++ controllers/thought-controller.js
@@ -0,0 +1,120 @@
+const {Thought, User} = require('../models');
+
+const thoughtController = {
+
+    // Add thought to the User
+    addThought({ params, body }, res) {
+        console.log(body);
+        Thought.create(body)
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+              { _id: params.userId },
+              { $push: { thoughts: _id } },
+              { new: true }
+            );
+          })
+          .then(dbThoughtData => {
+            if (!dbThoughtData) {
+              res.status(404).json({ message: 'No user found with this id!' });
+              return;
+            }
+            res.json(dbThoughtData);
+          })
+          .catch(err => res.json(err));
+      },
+
+      // GET all thoughts
+      getAllThoughts(req, res) {
+        Thought.find({})
+          .select('-__v')
+          .sort({_id: -1})
+          .then(dbThoughtData => res.json(dbThoughtData))
+          .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+      },
+
+      // GET thought
+      getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+         .select('-__v')
+          .then(dbThoughtData => {
+            if (!dbThoughtData) {
+              res.status(404).json({ message: 'No Thought found with this id!' });
+              return;
+            }
+            res.json(dbThoughtData);
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+      },
+
+      // Update thought
+      updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate({ _id: params.id }, body, { new: true })
+          .then(dbThoughtData => {
+            if (!dbThoughtData) {
+              res.status(404).json({ message: 'No Thought found with this id!' });
+              return;
+            }
+            res.json(dbThoughtData);
+          })
+          .catch(err => res.status(400).json(err));
+      },
+
+      // Remove thought
+      removeThought({ params }, res) {
+        Thought.findOneAndDelete({ _id: params.thoughtId })
+          .then(deletedThought => {
+            if (!deletedThought) {
+              return res.status(404).json({ message: 'No thought with this id!' });
+            }
+            return User.findOneAndUpdate(
+              { _id: params.userId },
+              { $pull: { thoughts: params.thoughtId } },
+              { new: true }
+            );
+          })
+          .then(dbThoughtData => {
+            if (!dbThoughtData) {
+              res.status(404).json({ message: 'No user found with this id!' });
+              return;
+            }
+            res.json(dbThoughtData);
+          })
+          .catch(err => res.json(err));
+      },
+
+      // Add reaction to thought
+      addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+          { _id: params.thoughtId },
+          { $push: { reactions: body } },
+          { new: true, runValidators: true }
+        )
+          .then(dbReactionData => {
+            if (!dbReactionData) {
+              res.status(404).json({ message: 'No user found with this id!' });
+              return;
+            }
+            res.json(dbReactionData);
+          })
+          .catch(err => res.json(err));
+      },
+
+      // Remove reaction from thought
+      removeReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+          { _id: params.thoughtId },
+          { $pull: { reactions: { reactionId: params.reactionId } } },
+          { new: true }
+        )
+          .then(dbReactionData => res.json(dbReactionData))
+          .catch(err => res.json(err));
+      },
+};
+
+module.exports = thoughtController;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-//const thoughtRoutes = require('./thought-routes');
+const thoughtRoutes = require('./thought-routes');
 const UserRoutes = require('./user-routes');
 
 router.use('/users', UserRoutes);
-//router.use('/thoughts', thoughtRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
